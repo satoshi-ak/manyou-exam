09ba04a23e1c08bea900958ab829ece68add68ee
satoshi aoki <satozi.ak.3250@gmail.com>
2021-08-24 21:51:45 +0900

    init

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  describe '新規作成機能' do
-    context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
-        visit new_task_path
-        fill_in 'task_title', with: 'title'
-        fill_in 'task_content', with: 'content'
-        click_button
-        expect(page).to have_content 'title'
-      end
-    end
+  let!(:task) { FactoryBot.create(:task, title: 'task') }
+  before do
+    visit tasks_path
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
@@ -17,6 +10,16 @@ RSpec.describe 'タスク管理機能', type: :system do
         task = FactoryBot.create(:task, title: 'task')
         visit tasks_path
         expect(page).to have_content 'task'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task1 = Task.create(title: '1', content: 'context1', created_at: '2021/08/24' )
+        task2 = Task.create(title: '2', content: 'context2', created_at: '2021/08/25' )
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content '1'
+        expect(task_list[1]).to have_content '2'
       end
     end
   end
